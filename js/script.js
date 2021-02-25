a56b9d0b3907a42513dafeee649d6b97c0115804
Caterina Bertotti <bertotti4997@gmail.com>
2021-02-25 16:00:36 +0100

    Exercise

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Qual'è il tuo nome?");
+var surName = prompt("Qual'è il tuo cognome?");
+var color = prompt("Qual'è il tuo colore preferito?");
+
+document .getElementById('password') .innerHTML = "La tua password è " + firstName + surName + color + "27";
